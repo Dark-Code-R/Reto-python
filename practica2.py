74ad6598ba00cd5a72b8d9b7f3fcd5ddcdf30e42
Dark-Code-R <Rodrigo.darkcode@gmail.com>
2023-09-19 13:27:38 -0400

    Primer Commit

---
+++ practica2.py
@@ -0,0 +1,8 @@
+a = input("Ingrese el primer numero: ")
+b = input("Ingrese el segundo numero: ")
+#convertir si o si las entradas de numero
+a = float(a)
+b = float(b)
+resultado = a + b
+print(f"El resultado de la suma es: {resultado}")
+
---
+++ practica3.py
@@ -0,0 +1,5 @@
+#convertir Celsius a Fahrenheit. Fomula a usar==> ºF = ºC x 1.8 + 32
+c=input("Ingrese el Dato Celsius para convertir: ")
+Celsius=float(c)
+Fahrenheit= Celsius * 1.8 + 32
+print(f"El resultado convertido a Fahrenheit es : {Fahrenheit}°F")
